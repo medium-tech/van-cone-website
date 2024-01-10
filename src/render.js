dbdfce29efa202ce439a6009870bc06b77f3a75f
b rad c <bradcorlett@gmail.com>
2024-01-10 00:38:54 -0600

    add mobile css

--- src/render.js
+++ src/render.js
@@ -41,10 +41,12 @@ const vanConePackage = JSON.parse(fs.readFileSync(vanCone('package.json'), 'utf-
 //
 
 const linkMap = {
-    'getting-started.md': 'getting-started.html',
-    'api-reference.md': 'api-reference.html',
-    'component-guide.md': 'component-guide.html'
+    './getting-started.md': 'getting-started.html',
+    './api-reference.md': 'api-reference.html',
+    './component-guide.md': 'component-guide.html'
 }
+
+let linkPrefix = '/latest/';
 
 // Override function
 const walkTokens = (token) => {
@@ -54,7 +56,7 @@ const walkTokens = (token) => {
 
         // format urls to be relative to the root and point to .html insted of .md files
         Object.keys(linkMap).forEach(key => {
-            token.href = token.href.replace(key, linkMap[key])
+            token.href = token.href.replace(key, linkPrefix + linkMap[key])
         })
 
         // console.log('found link: ', token.href, newHref);
@@ -85,11 +87,11 @@ const renderPage = (sourcePath, outputPath, pageTitle) => {
     <link rel="stylesheet" href="/style.css" />
 </head>
 <body>
-    <img id="van-logo-right-side" src="../img/logo-615x892.png" alt="Van Cone Logo" />   
+    <img id="van-logo-right-side" src="/img/logo-615x892.png" alt="Van Cone Logo" />   
     <div id="sidebar">
         <a href="/"><h1>Van Cone</h1></a>
         <a id="van-logo-sidebar" href="/">
-            <img id="van-logo-sidebar-img" src="../img/logo-308x446.png" alt="Van Cone Logo" />
+            <img id="van-logo-sidebar-img" src="/img/logo-308x446.png" alt="Van Cone Logo" />
         </a>
         <ul>
             <li><a href="/latest/getting-started.html">Getting Started</a></li>
@@ -122,9 +124,11 @@ const renderPage = (sourcePath, outputPath, pageTitle) => {
 // execute build
 //
 
-renderPage(vanCone('docs/api-reference.md'), 'latest/api-reference.html', 'Van Cone | API Documentation');
-renderPage(vanCone('docs/getting-started.md'), 'latest/getting-started.html', 'Van Cone | Getting Started');
-renderPage(vanCone('docs/component-guide.md'), 'latest/component-guide.html', 'Van Cone | Component Guide');
+renderPage(vanCone('docs/api-reference.md'), linkPrefix + 'api-reference.html', 'Van Cone | API Documentation');
+renderPage(vanCone('docs/getting-started.md'), linkPrefix + 'getting-started.html', 'Van Cone | Getting Started');
+renderPage(vanCone('docs/component-guide.md'), linkPrefix + 'component-guide.html', 'Van Cone | Component Guide');
+
+fs.writeFileSync(build(linkPrefix + 'version.txt'), vanConePackage.version, { encoding: 'utf-8', flag: 'w+'});
 
 fs.copyFileSync(asset('./index.html'), build('index.html'));
 fs.copyFileSync(asset('./style.css'), build('style.css'));
@@ -133,9 +137,15 @@ if (!fs.existsSync(build('img'))) fs.mkdirSync(build('img'));
 fs.copyFileSync(asset('./img/logo-615x892.png'), build('./img/logo-615x892.png'));
 fs.copyFileSync(asset('./img/logo-308x446.png'), build('./img/logo-308x446.png'));
 
+// linkPrefix = '/version/' + vanConePackage.version + '/';
+// renderPage(vanCone('docs/api-reference.md'), linkPrefix + 'api-reference.html', 'Van Cone | API Documentation');
+// renderPage(vanCone('docs/getting-started.md'), linkPrefix + 'getting-started.html', 'Van Cone | Getting Started');
+// renderPage(vanCone('docs/component-guide.md'), linkPrefix + 'component-guide.html', 'Van Cone | Component Guide');
+
+
 // find all syntax highlighting class names
 const uniqueClassNames = [...new Set(allClassNames)];
 const hslsClassNames = uniqueClassNames.filter(className => className.includes('hljs'));
-console.log(hslsClassNames);
+// console.log(hslsClassNames);
 
 console.log(`Van Cone ${vanConePackage.version} website build complete!`)
