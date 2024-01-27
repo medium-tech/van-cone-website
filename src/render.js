a271362b2c9fbe8f2611aa2987c95950e241a103
b rad c <bradcorlett@gmail.com>
2024-01-26 23:26:02 -0600

    update links and van-cone version

--- src/render.js
+++ src/render.js
@@ -42,11 +42,12 @@ const vanConePackage = JSON.parse(fs.readFileSync(vanCone('package.json'), 'utf-
 
 const linkMap = {
     './getting-started.md': 'getting-started.html',
-    './api-reference.md': 'api-reference.html',
-    './component-guide.md': 'component-guide.html'
+    './routing-and-nav-guide.md': 'routing-and-nav-guide.html',
+    './component-guide.md': 'component-guide.html',
+    './api-reference.md': 'api-reference.html'
 }
 
-let linkPrefix = '/latest/';
+let linkPrefix = './';
 
 // Override function
 const walkTokens = (token) => {
@@ -84,19 +85,22 @@ const renderPage = (sourcePath, outputPath, pageTitle) => {
 <head>
     <meta charset="UTF-8" />
     <title>${pageTitle}</title>
-    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="./style.css" />
 </head>
 <body>
-    <img id="van-logo-right-side" src="/img/logo-615x892.png" alt="Van Cone Logo" />   
+    <img id="van-logo-right-side" src="./img/logo-615x892.png" alt="Van Cone Logo" />   
     <div id="sidebar">
         <a href="/"><h1>Van Cone</h1></a>
         <a id="van-logo-sidebar" href="/">
-            <img id="van-logo-sidebar-img" src="/img/logo-308x446.png" alt="Van Cone Logo" />
+            <img id="van-logo-sidebar-img" src="./img/logo-308x446.png" alt="Van Cone Logo" />
         </a>
         <ul>
-            <li><a href="/latest/getting-started.html">Getting Started</a></li>
-            <li><a href="/latest/component-guide.html">Component Guide</a></li>
-            <li><a href="/latest/api-reference.html">API Reference</a></li>
+            <li><a href="./getting-started.html">Getting Started</a></li>
+            <li><a href="./routing-and-nav-guide.html">Routing and Nav Guide</a></li>
+            <li><a href="./component-guide.html">Component Guide</a></li>
+            <li><a href="./api-reference.html">API Reference</a></li>
+            <li><a href="https://github.com/b-rad-c/van-cone" target="_blank">Source code</a></li>
+            <li><a href="https://www.npmjs.com/package/van-cone" target="_blank">npm package</a></li>
         </ul>
     </div>
     <div id="content">
@@ -125,8 +129,9 @@ const renderPage = (sourcePath, outputPath, pageTitle) => {
 //
 
 renderPage(vanCone('docs/api-reference.md'), linkPrefix + 'api-reference.html', 'Van Cone | API Documentation');
-renderPage(vanCone('docs/getting-started.md'), linkPrefix + 'getting-started.html', 'Van Cone | Getting Started');
+renderPage(vanCone('docs/routing-and-nav-guide.md'), linkPrefix + 'routing-and-nav-guide.html', 'Van Cone | Routing and Nav Guide');
 renderPage(vanCone('docs/component-guide.md'), linkPrefix + 'component-guide.html', 'Van Cone | Component Guide');
+renderPage(vanCone('docs/getting-started.md'), linkPrefix + 'getting-started.html', 'Van Cone | Getting Started');
 
 fs.writeFileSync(build(linkPrefix + 'version.txt'), vanConePackage.version, { encoding: 'utf-8', flag: 'w+'});
 
